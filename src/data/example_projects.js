b449f55c6844eb19c2e7091a0b331b03341fa800
Nickolans Griffith <nickolansgriffith@Nicks-Macbook.local>
2022-02-09 22:36:06 -0600

    began task cell

---
+++ src/data/example_projects.js
@@ -0,0 +1,16 @@
+const Projects = [
+  {
+    name: "personal",
+    identifier: "dedae693-1957-45d0-92f9-d3635125432b"
+  },
+  {
+    name: "nicks cool app",
+    identifier: "d5b66b87-91b4-4886-99ca-7d625da71e93"
+  },
+  {
+    name: "my work :/",
+    identifier: "6eda08a5-5524-4939-b43d-13de6190807b"
+  }
+]
+
+export default Projects;
